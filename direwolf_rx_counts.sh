37fdace6cb89710d6a96e0ceec506b517f488cc0
Casey J. Davis <49321778+caseyjamesdavis@users.noreply.github.com>
2020-01-22 06:05:35 -0500

    implemented while and for loops
    while loop allows program to run without the use of the watch command
    for loop reduces multiple awk commands to one
    updated screenshot

--- direwolf_rx_counts.sh
+++ direwolf_rx_counts.sh
@@ -2,49 +2,40 @@
 
 # crude bash script to count the numbers of packets rx'd by direwolf
 # assumes direwolf is using the daily log file option, i.e., lowercase l (-l)
-# run this script using watch:
-#	 watch -n 15 ./direwolf_rx_counts.sh
 
-# to do:
-# 	hourly counts are a bit useless after the log file rolls over
-# 		i should consider using the single log file option in direwolf
+while :
+do
+	clear
 
+	# path to location of daily log files
+	direwolf_log_dir="$HOME/direwolf_rx_log"
 
-# path to location of daily log files
-direwolf_log_dir="$HOME/direwolf_rx_log"
+	# finds latest log file
+	filename=$(ls $direwolf_log_dir -t | head -n1)
+	echo "Data Source:"
+	echo $filename
+	echo ""
 
-# finds latest log file
-filename=$(ls $direwolf_log_dir -t | head -n1)
-echo "Data Source:"
-echo $filename
+	# defines full path to latest log file
+	full_path="$direwolf_log_dir/$filename"
 
-# defines full path to latest log file
-full_path="$direwolf_log_dir/$filename"
+	dt=("now-24 hour" "now-3 hour" "now-1 hour" "now-10 minute" "now-1 minute")
+	label=("Total in Log:" "Last 3 Hrs:" "Last 1 Hr:" "Last 10 Mins:" "Last Min:")
 
+	for i in ${!dt[@]};
+	do
 
-# determines and prints the total number of packets recieved
-echo " "
-echo "Total in Log:"
-#wc -l ./aprs_log/$filename | cut -d ' ' -f 1
-awk -F',' -vDate=`date -d'now-24 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $full_path | wc -l
+	  t_setting=${dt[$i]}
+	  description=${label[$i]}
 
+	  echo $description
 
-# number of packets rx'd in the last 3 hours
-echo " "
-echo "Last 3 Hours:"
-awk -F',' -vDate=`date -d'now-3 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $full_path | wc -l
+	  awk -F',' -vDate=`date -d "$t_setting" +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $full_path | wc -l
 
-# number of packets rx'd in the last hour
-echo " "
-echo "Last Hour:"
-awk -F',' -vDate=`date -d'now-1 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $full_path | wc -l
+	  echo ""
 
-# number of packets rx'd in the last 10 minutes
-echo " "
-echo "Last 10 Min:"
-awk -F',' -vDate=`date -d'now-10 minute' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $full_path | wc -l
+	done
 
-# number of packets rx'd in the last minute
-echo " "
-echo "Last Min:"
-awk -F',' -vDate=`date -d'now-1 minute' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $full_path | wc -l
+	sleep 10s
+
+done
