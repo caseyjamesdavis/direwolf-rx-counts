5c8968f17a20243e56013eb9dfdb86ff8cc8a2ad
Casey J. Davis <49321778+caseyjamesdavis@users.noreply.github.com>
2020-01-23 22:34:00 -0500

    use two log files as input
    allows hourly counts to be usable after log file rolls over each day

--- direwolf_rx_counts.sh
+++ direwolf_rx_counts.sh
@@ -10,18 +10,20 @@ do
 	# path to location of daily log files
 	direwolf_log_dir="$HOME/direwolf_rx_log"
 
-	# finds latest log file
-	filename=$(ls $direwolf_log_dir -t | head -n1)
-	echo "Data Source:"
-	echo $filename
+	# finds two most recent log files in directory
+	second_newest_file=$(ls $direwolf_log_dir -Art | tail -n 2 | head -n 1)
+	first_newest_file=$(ls $direwolf_log_dir -Art | tail -n 2 | tail -n 1)
+
+	echo "Data Sources:"
+	echo $second_newest_file
+	echo $first_newest_file
 	echo ""
 
-	# defines full path to latest log file
-	full_path="$direwolf_log_dir/$filename"
-
+	# define list of times for awk command and labels for output
 	dt=("now-24 hour" "now-3 hour" "now-1 hour" "now-10 minute" "now-1 minute")
-	label=("Total in Log:" "Last 3 Hrs:" "Last 1 Hr:" "Last 10 Mins:" "Last Min:")
+	label=("Last 24 Hrs:" "Last 3 Hrs:" "Last 1 Hr:" "Last 10 Mins:" "Last Min:")
 
+	# loop to run awk command for each time defined above
 	for i in ${!dt[@]};
 	do
 
@@ -30,20 +32,20 @@ do
 
 		echo $description
 
-		awk -F',' -vDate=`date -d "$t_setting" +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $full_path | wc -l
+		cat $direwolf_log_dir/$second_newest_file $direwolf_log_dir/$first_newest_file | awk -F',' -vDate=`date -d "$t_setting" +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' | wc -l
 
 		echo ""
 
 	done
 
-
+	# get names of 'source' from logs, i.e., stations which were heard directly
 	echo ""
         echo "Rx Sources in the Last 3 Hrs:"
 	echo ""
 	
-	awk -F',' -vDate=`date -d 'now-3 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $5}' $full_path | sort | uniq -c | sort -r
+	cat $direwolf_log_dir/$second_newest_file $direwolf_log_dir/$first_newest_file | awk -F',' -vDate=`date -d 'now-3 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $5}' | sort | uniq -c | sort -r
 
-
+	# pause infinite loop
 	sleep 10s
 
 done
