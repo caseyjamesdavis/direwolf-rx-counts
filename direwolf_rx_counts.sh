fc8f38a3310aed81b0e1608bf79c5b564014e45e
Casey J. Davis <49321778+caseyjamesdavis@users.noreply.github.com>
2020-01-25 09:51:24 -0500

    removed multipe cat commands
    now using awk's ability to read multiple files
    created variable for paths of two newest log files to help keep awk commands readable

--- direwolf_rx_counts.sh
+++ direwolf_rx_counts.sh
@@ -2,15 +2,17 @@
 
 # crude bash script to count the number of packets rx'd by direwolf
 # assumes direwolf is using the daily log file option, i.e., lowercase l (-l)
+# Casey J. Davis 2020.01.25
 
+# forever loop
 while :
 do
 	clear
 
-	# path to location of daily log files
+	# location of daily log files
 	direwolf_log_dir="$HOME/direwolf_rx_log"
 
-	# finds two most recent log files in directory
+	# finds two most recent log files
 	second_newest_file=$(ls $direwolf_log_dir -Art | tail -n 2 | head -n 1)
 	first_newest_file=$(ls $direwolf_log_dir -Art | tail -n 2 | tail -n 1)
 
@@ -19,11 +21,14 @@ do
 	echo $first_newest_file
 	echo ""
 
+	# define complete paths to both log files
+	two_files="$direwolf_log_dir/$second_newest_file $direwolf_log_dir/$first_newest_file"
+
 	# define list of times for awk command and labels for output
 	dt=("now-24 hour" "now-3 hour" "now-1 hour" "now-10 minute" "now-1 minute")
 	label=("Last 24 Hrs:" "Last 3 Hrs:" "Last 1 Hr:" "Last 10 Mins:" "Last Min:")
 
-	# loop to run awk command for each time defined above
+	# loop to run awk command for each time defined above and for both log files
 	for i in ${!dt[@]};
 	do
 
@@ -32,7 +37,7 @@ do
 
 		echo $description
 
-		cat $direwolf_log_dir/$second_newest_file $direwolf_log_dir/$first_newest_file | awk -F',' -vDate=`date -d "$t_setting" +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' | wc -l
+		awk -F',' -vDate=`date -d "$t_setting" +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $two_files | wc -l
 
 		echo ""
 
@@ -43,7 +48,7 @@ do
         echo "Rx Sources in the Last 3 Hrs:"
 	echo ""
 	
-	cat $direwolf_log_dir/$second_newest_file $direwolf_log_dir/$first_newest_file | awk -F',' -vDate=`date -d 'now-3 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $5}' | sort | uniq -c | sort -r
+	awk -F',' -vDate=`date -d 'now-3 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $5}' $two_files | sort | uniq -c | sort -r
 
 	# pause infinite loop
 	sleep 10s
