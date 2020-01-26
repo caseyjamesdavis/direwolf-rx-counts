af3091ef4dd345066e434d625d0688fbe09ce6a4
Casey J. Davis <49321778+caseyjamesdavis@users.noreply.github.com>
2020-01-26 11:58:43 -0500

    change from echo to printf
    also removed some pointless intermediate variables
    updated screenshot

--- direwolf_rx_counts.sh
+++ direwolf_rx_counts.sh
@@ -2,7 +2,7 @@
 
 # crude bash script to count the number of packets rx'd by direwolf
 # assumes direwolf is using the daily log file option, i.e., lowercase l (-l)
-# Casey J. Davis 2020.01.25
+# Casey J. Davis 2020.01.26
 
 # forever loop
 while :
@@ -16,11 +16,9 @@ do
 	second_newest_file=$(ls $direwolf_log_dir | sort -m | tail -n 2 | head -n 1)
 	first_newest_file=$(ls $direwolf_log_dir | sort -m | tail -n 2 | tail -n 1)
 
-	echo "Data Sources:"
-	echo $second_newest_file
-	echo $first_newest_file
-	echo ""
-
+	printf "%-15s %-15s\n" "Data Sources:" $second_newest_file
+	printf "%-15s %-15s\n\n" "" $first_newest_file 
+	
 	# define complete paths to both log files
 	two_files="$direwolf_log_dir/$second_newest_file $direwolf_log_dir/$first_newest_file"
 
@@ -32,23 +30,19 @@ do
 	for i in ${!dt[@]};
 	do
 
-		t_setting=${dt[$i]}
-		description=${label[$i]}
+		count=$(awk -F',' -vDate=`date -d "${dt[$i]}" +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $two_files | wc -l)
 
-		echo $description
-
-		awk -F',' -vDate=`date -d "$t_setting" +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $0}' $two_files | wc -l
-
-		echo ""
+		printf "%-15s %-5i\n\n" "${label[$i]}" $count
 
 	done
 
 	# get names of 'source' from logs, i.e., stations which were heard directly
-	echo ""
-        echo "Rx Sources in the Last 3 Hrs:"
-	echo ""
-	
-	awk -F',' -vDate=`date -d 'now-3 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $5}' $two_files | sort | uniq -c | sort -r
+
+	rx_sources=$(awk -F',' -vDate=`date -d 'now-3 hour' +'%Y-%m-%dT%H:%M:%SZ'` '/chan/ {next} $3 > Date {print $5}' $two_files | sort | uniq -c | sort -r)
+
+	printf "%-25s\n\n" "Rx Sources in the Last 3 Hrs:"
+
+	printf "%s\n\n" "$rx_sources"
 
 	# pause infinite loop
 	sleep 10s
